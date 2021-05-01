566e5a3b9a850f21a5a8bdc6a591e480d4f49bb7
emnakorbi <57458808+emnakorbi@users.noreply.github.com>
2021-05-01 23:01:40 +0100

    modification claim

--- src/app/services/claimservice.service.ts
+++ src/app/services/claimservice.service.ts
@@ -1,10 +1,11 @@
+
 import { Injectable } from '@angular/core';
-import {User} from "../models/user";
+
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {claims} from "../models/claims";
 import {environment} from "../../environments/environment";
-import {Subscription} from "../models/subscription";
+
 
 @Injectable({
   providedIn: 'root'
